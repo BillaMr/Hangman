10b16c17657f28e2b38357c88865164185c85f1f
Manoj Billa <bmanojreddy9@gmail.com>
2016-10-20 11:28:42 -0400

    Initial Changes

---
+++ Hangman1/Hangman1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hangman1.Startup))]
+namespace Hangman1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
